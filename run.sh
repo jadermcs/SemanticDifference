cd6d51bc31bceb3ba6dd8eee81f006489b2d1f5d
Jader <jader.martins@list.lu>
2025-04-14 15:29:10 +0200

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --target
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --supersense
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --target
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --supersense
-python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --target --supersense
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --wandb_run_name "roberta-base-wic"
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --wandb_run_name "roberta-base-wic-mask"
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --target --wandb_run_name "roberta-base-wic-target"
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --supersense --wandb_run_name "roberta-base-wic-supersense"
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --target --wandb_run_name "roberta-base-wic-mask-target"
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --supersense --wandb_run_name "roberta-base-wic-mask-supersense"
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --target --supersense --wandb_run_name "roberta-base-wic-target-supersense"
 
 # python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug
 # python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --mask
