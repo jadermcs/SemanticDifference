29c3f8029e5065228a66eba4dfcf6c65b48179e2
Jader <jader.martins@list.lu>
2025-04-14 15:27:20 +0200

    update

---
+++ run.sh
@@ -0,0 +1,41 @@
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --target
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --supersense
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --target
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --mask --supersense
+python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wic --target --supersense
+
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --mask
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --target
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --supersense
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --mask --target
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --mask --supersense
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset dwug --target --supersense
+
+
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet --mask
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet --target
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet --supersense
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet --mask --target
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet --mask --supersense
+# python bert-weak-supervision.py --model FacebookAI/roberta-base --dataset wordnet --target --supersense
+
+
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic --mask
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic --target
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic --supersense
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic --mask --target
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic --mask --supersense
+# python bert-weak-supervision.py --model answerdotai/ModernBERT-base --dataset wic --target --supersense
+
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic --mask
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic --target
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic --supersense
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic --mask --target
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic --mask --supersense
+# python bert-weak-supervision.py --model microsoft/deberta-v3-base --dataset wic --target --supersense
