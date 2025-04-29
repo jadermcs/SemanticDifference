fc650a070cbaf275c38a574c78cef983321558d5
Jader <jader.martins@list.lu>
2025-04-29 20:41:40 +0200

    update

--- src/bert_trainer3.py
+++ src/bert_trainer3.py
@@ -100,6 +100,34 @@ def load_data(datasets, split="train", mark_target=False, supersense=False):
     return Dataset.from_list(processed_data)
 
 
+def mask_tokens(inputs, tokenizer, mlm_probability=.15):
+    """
+    Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+    """
+    # We sample a few tokens in each sequence for MLM training (with probability self.mlm_probability)
+    labels = inputs.clone()
+    probability_matrix = torch.full(labels.shape, mlm_probability)
+    special_tokens_mask = [
+        tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+    ]
+    special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+
+    probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+    masked_indices = torch.bernoulli(probability_matrix).bool()
+    labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+    # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.mask_token_id
+
+    # 10% of the time, we replace masked input tokens with random word
+    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    inputs[indices_random] = random_words[indices_random]
+
+    # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+    return inputs, labels
+
 def preprocess_function(examples, tokenizer, supersense=False):
     """Tokenize the input sentences."""
     tokens = tokenizer(
@@ -109,6 +137,7 @@ def preprocess_function(examples, tokenizer, supersense=False):
         max_length=MAX_LENGTH,
         padding='max_length',
         return_token_type_ids=True,
+        return_tensors='pt',
         is_split_into_words=supersense
     )
     if supersense:
@@ -129,6 +158,7 @@ def preprocess_function(examples, tokenizer, supersense=False):
                 new_supersenses.append(supersenses[word_id])
         tokens['token_labels'] = new_supersenses
 
+    tokens['input_ids'], tokens['mlm_labels'] = mask_tokens(tokens['input_ids'], tokenizer)
     tokens['labels'] = examples['labels']
     return tokens
 
