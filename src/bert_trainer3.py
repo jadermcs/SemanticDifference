6975e11b37976c2a6e507f4e5386eb02a97def9e
Jader <jader.martins@list.lu>
2025-04-30 05:01:48 +0200

    update

--- src/bert_trainer3.py
+++ src/bert_trainer3.py
@@ -100,31 +100,56 @@ def load_data(datasets, split="train", mark_target=False, supersense=False):
     return Dataset.from_list(processed_data)
 
 
+# def mask_tokens(inputs, tokenizer, mlm_probability=.15):
+#     """
+#     Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+#     """
+#     # We sample a few tokens in each sequence for MLM training (with probability self.mlm_probability)
+#     labels = inputs.clone()
+#     probability_matrix = torch.full(labels.shape, mlm_probability)
+#     special_tokens_mask = tokenizer.get_special_tokens_mask(labels, already_has_special_tokens=True)
+#     special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+
+#     probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+#     masked_indices = torch.bernoulli(probability_matrix).bool()
+#     labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+#     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+#     indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+#     inputs[indices_replaced] = tokenizer.mask_token_id
+
+#     # 10% of the time, we replace masked input tokens with random word
+#     indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+#     random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+#     inputs[indices_random] = random_words[indices_random]
+
+#     # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+#     return inputs, labels
+
+
 def mask_tokens(inputs, tokenizer, mlm_probability=.15):
     """
     Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
     """
     # We sample a few tokens in each sequence for MLM training (with probability self.mlm_probability)
-    labels = inputs.clone()
-    probability_matrix = torch.full(labels.shape, mlm_probability)
-    special_tokens_mask = tokenizer.get_special_tokens_mask(labels, already_has_special_tokens=True)
+    probability_matrix = torch.full(inputs.shape, mlm_probability)
+    special_tokens_mask = tokenizer.get_special_tokens_mask(inputs, already_has_special_tokens=True)
     special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
 
     probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
     masked_indices = torch.bernoulli(probability_matrix).bool()
-    labels[~masked_indices] = -100  # We only compute loss on masked tokens
 
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+    indices_replaced = torch.bernoulli(torch.full(inputs.shape, 0.8)).bool() & masked_indices
     inputs[indices_replaced] = tokenizer.mask_token_id
 
     # 10% of the time, we replace masked input tokens with random word
-    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
-    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    indices_random = torch.bernoulli(torch.full(inputs.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(len(tokenizer), inputs.shape, dtype=torch.long)
     inputs[indices_random] = random_words[indices_random]
 
     # The rest of the time (10% of the time) we keep the masked input tokens unchanged
-    return inputs, labels
+    return inputs
 
 def preprocess_function(examples, tokenizer, supersense=False):
     """Tokenize the input sentences."""
@@ -157,7 +182,7 @@ def preprocess_function(examples, tokenizer, supersense=False):
         tokens['token_labels'] = new_supersenses
 
     # tokens['input_ids'], tokens['mlm_labels'] = mask_tokens(tokens['input_ids'][0], tokenizer)
-    tokens['input_ids'], _ = mask_tokens(tokens['input_ids'][0], tokenizer)
+    tokens['input_ids'] = mask_tokens(tokens['input_ids'][0], tokenizer)
     tokens['labels'] = examples['labels']
     return tokens
 
